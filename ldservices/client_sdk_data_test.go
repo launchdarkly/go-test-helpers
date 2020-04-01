52d23c754d83bb7a127102c2a8c5ace5fdeffe64
Eli Bishop <eli@launchdarkly.com>
2020-04-01 10:25:02 -0700

    add handlers to simulate LD client-side stream

---
+++ ldservices/client_sdk_data_test.go
@@ -0,0 +1,62 @@
+package ldservices
+
+import (
+	"encoding/json"
+	"testing"
+
+	"gopkg.in/launchdarkly/go-sdk-common.v2/ldtime"
+
+	"gopkg.in/launchdarkly/go-sdk-common.v2/ldreason"
+	"gopkg.in/launchdarkly/go-sdk-common.v2/ldvalue"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmptyClientSDKData(t *testing.T) {
+	expectedJSON := `{}`
+	data := NewClientSDKData()
+	bytes, err := json.Marshal(data)
+	assert.NoError(t, err)
+	assertJSONEqual(t, expectedJSON, string(bytes))
+}
+
+func TestClientSDKDataWithFlags(t *testing.T) {
+	flag1 := FlagValueData{
+		Key:                  "flagkey1",
+		Version:              1,
+		FlagVersion:          1000,
+		Value:                ldvalue.String("a"),
+		VariationIndex:       2,
+		Reason:               ldreason.NewEvalReasonFallthrough(),
+		TrackEvents:          true,
+		DebugEventsUntilDate: ldtime.UnixMillisecondTime(3000),
+	}
+	flag2 := FlagValueData{
+		Key:            "flagkey2",
+		Version:        2,
+		FlagVersion:    2000,
+		Value:          ldvalue.String("b"),
+		VariationIndex: -1,
+	}
+	data := NewClientSDKData().Flags(flag1, flag2)
+
+	expectedJSON := `{
+		"flagkey1": {
+			"version": 1,
+			"flagVersion": 1000,
+			"value": "a",
+			"variation": 2,
+			"reason": { "kind": "FALLTHROUGH" },
+			"trackEvents": true,
+			"debugEventsUntilDate": 3000
+		},
+		"flagkey2": {
+			"version": 2,
+			"flagVersion": 2000,
+			"value": "b"
+		}
+	}`
+	bytes, err := json.Marshal(data)
+	assert.NoError(t, err)
+	assertJSONEqual(t, expectedJSON, string(bytes))
+}
